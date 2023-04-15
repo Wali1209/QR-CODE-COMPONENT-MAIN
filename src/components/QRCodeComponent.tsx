f5b5b624adb6b96250ac39cf881c4d46e3d8548c
wali1209 <walibilal1209@gmail.com>
2023-04-15 15:58:50 +0500

    React app Created!

---
+++ src/components/QRCodeComponent.tsx
@@ -0,0 +1,5 @@
+function QRCodeComponent() {
+  const frontEndMentor = "www.frontendmentor.io/";
+  const srcFile = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${frontEndMentor}`;
+  
+}
