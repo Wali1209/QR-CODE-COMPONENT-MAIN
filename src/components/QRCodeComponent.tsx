67366e5f5b0feb42a901cccbb7d03e4c5208d3d9
wali1209 <walibilal1209@gmail.com>
2023-04-26 00:51:00 +0500

    src attribute of img tag modified! loading local image instead of API

--- src/components/QRCodeComponent.tsx
+++ src/components/QRCodeComponent.tsx
@@ -6,12 +6,10 @@ interface imgProps {
 }
 
 function QRCodeComponent(props: imgProps) {
-  const frontEndMentor = "www.frontendmentor.io/";
-  const srcFile = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${frontEndMentor}`;
   return (
     <Container>
       <StyledQRCodeComponent>
-        <img src={srcFile} alt={props.imgAlt} />
+        <img src="../../images/image-qr-code.png" alt={props.imgAlt} />
         <h3>Improve your front-end skills by building projects</h3>
         <p>
           Scan the QR code to visit Frontend Mentor and take your coding skills
