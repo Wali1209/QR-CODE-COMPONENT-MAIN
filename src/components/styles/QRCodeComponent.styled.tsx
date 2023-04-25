4aeec2f48fa23840d7706438e0fa30063f2ecb9f
wali1209 <walibilal1209@gmail.com>
2023-04-26 00:52:21 +0500

    QRCodeComponent.styled updated updated!

--- src/components/styles/QRCodeComponent.styled.tsx
+++ src/components/styles/QRCodeComponent.styled.tsx
@@ -1,36 +1,32 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
+export const Container = styled.div`
   width: 100%;
-  height: calc(100vh);
-  border: 2px solid black;
-  background-color: hsl(220, 15%, 55%);
+  min-height: 100vh;
+  background-color: #d5e1ef;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0 auto;
 `;
 
 export const StyledQRCodeComponent = styled.div`
-  width: 300px;
+  width: 20rem;
   text-align: center;
   background-color: white;
   border-radius: 10px;
-  box-shadow: 0 8px 10px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 1px 20px rgba(0, 0, 0, 0.1);
+  padding: 1rem;
 
-  div {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 50px;
-    background-color: rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
+  img {
+    border-radius: 0.75rem;
   }
   h3 {
-    font-size: 20px;
-    margin: 12px 20px;
+    font-size: 1.25rem;
+    margin: 1rem;
   }
   p {
-    margin: 20px 20px 30px;
+    font-size: 1rem;
+    opacity: 0.6;
+    margin: 0 1rem 1.5rem;
   }
 `;
